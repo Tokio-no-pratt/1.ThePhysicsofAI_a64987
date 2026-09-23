83e30dc481138601ee166626cd8e7d54571184d4
Tokio <suelennascimento164@gmail.com>
2026-09-23 21:56:40 +0100

    trabalho finalizado
    fim

--- Assets/Script/UpdateMove.cs
+++ Assets/Script/UpdateMove.cs
@@ -1,14 +1,16 @@
 ﻿
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UpdateMove : MonoBehaviour
 {
+    public float speed = 0.5f;
 
     void Update()
     {
-        this.transform.Translate(0, 0, Time.deltaTime);
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
     }
 }
 
