35619fd90473fc82d494bddf71b254207d45cc04
Tokio <suelennascimento164@gmail.com>
2026-09-23 15:32:12 +0100

    Initial commit

---
+++ Assets/Script/FixedUpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedUpdateMove : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.transform.Translate(0, 0, 0.01f);
+    }
+}
+
---
+++ Assets/Script/LateUpdateMove.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateUpdateMove : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        this.transform.Translate(0, 0, 0.01f);
+    }
+
+
+}
+
+
+
